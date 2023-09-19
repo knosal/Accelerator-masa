d3bec65c16db1359de1af7c1bcf4516c156dc9b9
Knosal.V <35274816+knosal@users.noreply.github.com>
2023-09-19 17:14:36 +0300

    Реализация блоков header / hero / about

---
+++ source/js/modules/menuToggle.js
@@ -0,0 +1,47 @@
+const initButtonMenu = () => {
+  const isOpened = 'is-opened';
+  const header = document.querySelector('.header');
+  const container = header.querySelector('.header__container');
+  const toggleBtn = header.querySelector('.navigation__button');
+  const navigation = header.querySelector('.navigation');
+
+  const onDocumentKeydown = (evt) => {
+    return evt.key === 'Escape' ? closeMenu() : null;
+  };
+
+  const onLinkClick = (evt) => {
+    return evt.target.matches('.navigation__scroll') ? closeMenu() : null;
+  };
+
+  const isMenu = (evt) => {
+    return (evt.target.closest('.header') && evt.target.closest('.navigation__button')) || evt.target.closest('.navigation') ? evt.preventDefault() : closeMenu();
+  };
+
+  const openMenu = () => {
+    header.classList.add(isOpened);
+    container.classList.add(isOpened);
+    toggleBtn.classList.add(isOpened);
+    navigation.classList.add(isOpened);
+    document.addEventListener('keydown', onDocumentKeydown);
+    navigation.addEventListener('click', onLinkClick);
+    document.addEventListener('click', isMenu);
+    window.scrollLock.disableScrolling();
+  };
+
+  const closeMenu = () => {
+    header.classList.remove(isOpened);
+    container.classList.remove(isOpened);
+    toggleBtn.classList.remove(isOpened);
+    navigation.classList.remove(isOpened);
+    document.removeEventListener('keydown', onDocumentKeydown);
+    navigation.removeEventListener('click', onLinkClick);
+    document.removeEventListener('click', isMenu);
+    window.scrollLock.enableScrolling();
+  };
+
+  toggleBtn.addEventListener('click', () => {
+    return !toggleBtn.classList.contains(isOpened) ? openMenu() : closeMenu();
+  });
+};
+
+export {initButtonMenu};
