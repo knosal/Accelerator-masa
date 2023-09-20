c13c0ceac0299137c0278e62d68d7725831697a9
Knosal.V <35274816+knosal@users.noreply.github.com>
2023-09-20 15:05:54 +0300

    Добавление manifest в build / Верстка блоков

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,24 @@
+// Импортируем необходимые модули и функции Gulp
 import gulp from 'gulp';
 import browserSync from 'browser-sync';
 import del from 'del';
+
+// Импортируем функции для обработки стилей и других задач из других файлов
 import {compileStyles, compileMinStyles} from './gulp/compileStyles.mjs';
-import { copy, copyImages, copySvg } from './gulp/copyAssets.mjs';
+import {copy, copyImages, copySvg} from './gulp/copyAssets.mjs';
 import {compileMainMinScripts, compileMainScripts, compileVendorScripts} from './gulp/compileScripts.mjs';
 import {optimizeSvg, sprite, createWebp, optimizePng, optimizeJpg} from './gulp/optimizeImages.mjs';
 
+// Создаем экземпляр сервера BrowserSync
 const server = browserSync.create();
+
+// Функция для обновления стилей без перезагрузки страницы
 const streamStyles = () => compileStyles().pipe(server.stream());
 
+// Функция для удаления директории 'build'
 const clean = () => del('build');
 
+// Функция для настройки и запуска локального сервера
 const syncServer = () => {
   server.init({
     server: 'build/',
@@ -21,6 +29,7 @@ const syncServer = () => {
     ui: false,
   });
 
+  // Наблюдение за файлами и выполнение соответствующих задач при изменениях
   gulp.watch('source/**.html', gulp.series(copy, refresh));
   gulp.watch('source/sass/**/*.{scss,sass}', streamStyles);
   gulp.watch('source/js/**/*.{js,json}', gulp.series(compileMainScripts, compileVendorScripts, refresh));
@@ -34,13 +43,20 @@ const syncServer = () => {
   gulp.watch('source/*.php', gulp.series(copy, refresh));
 };
 
+// Функция для обновления страницы
 const refresh = (done) => {
   server.reload();
   done();
 };
 
+// Функция для сборки проекта
 const build = gulp.series(clean, copy, sprite, gulp.parallel(compileMinStyles, compileMainMinScripts, compileVendorScripts, optimizePng, optimizeJpg, optimizeSvg));
+
+// Функция для разработки
 const dev = gulp.series(clean, copy, sprite, gulp.parallel(compileMinStyles, compileMainMinScripts, compileVendorScripts, optimizePng, optimizeJpg, optimizeSvg), syncServer);
+
+// Функция для старта разработки
 const start = gulp.series(clean, copy, sprite, gulp.parallel(compileStyles, compileMainScripts, compileVendorScripts), syncServer);
 
-export { createWebp as webp, build, start, dev};
+// Экспортируем функции для использования в Gulp
+export {createWebp as webp, build, start, dev};
